c30ddd91abb546c6caa66096c16652376922f511
Mario Gunawan <margunwa123@yahoo.com>
2019-11-05 22:09:55 +0700

    konak

--- src/extract.py
+++ src/extract.py
@@ -14,6 +14,8 @@ import pickle
 import os
 import cv2
 import numpy as np
+import similarity as sim
+import scipy
 
 # 1. Feature Detection and Description
 def extract_features(img_path, vector_size=64):
@@ -44,3 +46,75 @@ def batch_extractor(imgs_path, pickled_db_path="features.pck"):
         result[name] = extract_features(f)
     with open(pickled_db_path, 'wb') as fp:
         pickle.dump(result, fp)
+
+def closest_match_euc(img):
+    with open("features.pck",'rb') as fp:
+        data = pickle.load(fp)
+        matrix = []
+        for i in data.items():
+            matrix.append(i)
+        #matrix = np.array(matrix)
+        print("matrix:",matrix[2][1])
+        features = extract_features(img)
+        print("features:",features)
+        print(sim.cos_similarity(matrix[2][1],features))
+        print(sim.euclidean_distance(matrix[2][1],features))
+        mindist = sim.euclidean_distance(features,matrix[2][1])
+        print("mindist 1",mindist)
+        path = matrix[0][0]
+        print(path)
+        i = 0
+        for f in matrix:
+            A = sim.euclidean_distance(features,f[1])
+            if A < mindist:
+                mindist = A
+                path = f[0]
+            i += 1
+        return path
+
+def closest_match_cosine(img):
+    with open("features.pck",'rb') as fp:
+        data = pickle.load(fp)
+        matrix = []
+        for i in data.items():
+            matrix.append(i)
+        matrix = np.array(matrix)
+        
+        features = extract_features(img)
+        mindist = sim.cos_similarity(features,matrix[0])
+        path = matrix[0]
+        for f in matrix:
+            A = sim.cos_similarity(features,f)
+            if A < mindist:
+                mindist = A
+                path = f
+        return path
+
+"""
+class Matcher(object):
+    def __init__(self, pickled_db_path="features.pck"):
+        with open(pickled_db_path) as fp:
+            self.data = pickle.load(fp)
+        self.names = []
+        self.matrix = []
+        for k, v in self.data.iteritems():
+            self.names.append(k)
+            self.matrix.append(v)
+        self.matrix = np.array(self.matrix)
+        self.names = np.array(self.names)
+
+    def cos_cdist(self, vector):
+        v = vector.reshape(1, -1)
+        print("matrix pertama: "+ self.matrix[0])
+        print("name pertama: "+ self.names[0])
+        print("cos cdist:" + scipy.spatial.distance.cdist(self.matrix, v, 'cosine').reshape(-1))
+        return scipy.spatial.distance.cdist(self.matrix, v, 'cosine').reshape(-1) #ini tuh udah bentuk array
+
+    def match(self, image_path, topn=5):
+        features = extract_features(image_path)
+        img_distances = self.cos_cdist(features)
+        print("Img distance:" + img_distances)
+        nearest_ids = np.argsort(img_distances)[:topn].tolist() #buat ngesort cdist paling gede lalu dimasukin ke list
+        nearest_img_paths = self.names[nearest_ids].tolist() #buat ngedapetin nama path
+        return nearest_img_paths, img_distances[nearest_ids].tolist()
+"""
--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import extract
 import img
 import cv2
 
+path = extract.closest_match_euc("../img/Aaron Paul0_262.jpg")
+print(path)
+path2 = "..\\" + path
+img.show_img(path2,"hehe")
+cv2.waitKey()
+"""
 v1 = extract.extract_features("../img/Aaron Paul0_262.jpg")
 img.show_img("../img/Aaron Paul0_262.jpg",'first img')
 v2 = extract.extract_features("../img/Aaron Paul0_262.jpg")
@@ -10,6 +16,7 @@ img.show_img("../img/Aaron Paul0_262.jpg",'second img')
 #extract.batch_extractor("../img")
 cv2.waitKey()
 sim = similarity.cos_similarity(v1, v2)
-euc = similarity.euclidean_distance(v1, v2)
-print(sim)
-print(euc)
+$euc = similarity.euclidean_distance(v1, v2)
+>>print(sim)
+!print(euc)
+"""
